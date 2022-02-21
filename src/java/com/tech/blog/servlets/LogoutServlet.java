6e2f6cfa59e3af869ea6a1533fe602697c572872
Akash Dash <56639516+akash22-03@users.noreply.github.com>
2022-02-21 22:51:50 +0530

    Add files via upload

--- src/java/com/tech/blog/servlets/LogoutServlet.java
+++ src/java/com/tech/blog/servlets/LogoutServlet.java
@@ -1,10 +1,10 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
 package com.tech.blog.servlets;
 
-import com.tech.blog.dao.UserDao;
 import com.tech.blog.entities.Message;
-import com.tech.blog.entities.User;
-import com.tech.blog.helper.ConnectionProvider;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -13,41 +13,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ *
+ * @author akash
+ */
+public class LogoutServlet extends HttpServlet {
 
-public class LoginServlet extends HttpServlet {
-    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-
+            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("<title>Servlet LogoutServlet</title>");            
             out.println("</head>");
             out.println("<body>");
             
-            //fetch username and password from request
+            HttpSession s = request.getSession();
+            s.removeAttribute("currentUser");
+            Message m = new Message("Logout Successfully","success","alert-success");
+            s.setAttribute("msg",m);
+            response.sendRedirect("login_page.jsp");
             
-            String userEmail = request.getParameter("email");
-            String userPassword = request.getParameter("password");
-            
-            UserDao dao = new UserDao(ConnectionProvider.getConnection());
-            User u = dao.getUserByEmailAndPassword(userEmail, userPassword);
-            if(u==null){
-                //login error
-                //out.println("<h2>Error Occured.!!</h2>");
-                Message msg = new Message("Invalid Details.","error","alert-danger");
-                HttpSession s = request.getSession();
-                s.setAttribute("msg", msg);
-                response.sendRedirect("login_page.jsp");
-                
-            }else{
-                //login success
-                HttpSession s = request.getSession();
-                s.setAttribute("currentUser",u);
-                response.sendRedirect("profile.jsp");
-            }
             out.println("</body>");
             out.println("</html>");
         }
