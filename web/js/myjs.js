734cb953c78e72495663cb6e73ec2948b94b4906
Akash Dash <56639516+akash22-03@users.noreply.github.com>
2022-02-28 23:44:55 +0530

    Add files via upload

--- web/js/myjs.js
+++ web/js/myjs.js
@@ -1,4 +1,22 @@
-
-//$(document).ready(function(){
-//   alert("document ready")
-//})
+function doLike(pid,uid){
+    const d = {
+        uid:uid,
+        pid:pid,
+        operation :"like"
+    }
+    $.ajax({
+        url:"LikeServlet",
+        data: d,
+        success: function (data, textStatus, jqXHR) {
+            console.log(data);
+            if(data.trim()=='true'){
+                let c = $(".like-counter").html();
+                c++;
+                $(".like-counter").html(c);
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(data);
+        }
+    })
+}
